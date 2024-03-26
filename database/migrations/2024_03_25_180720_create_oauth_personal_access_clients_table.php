17a0eb8fbb97bdc8d506ac97860851192337a47e
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-26 15:39:31 +0000

    Update 2024_03_25_180720_create_oauth_personal_access_clients_table.php

--- database/migrations/2024_03_25_180720_create_oauth_personal_access_clients_table.php
+++ database/migrations/2024_03_25_180720_create_oauth_personal_access_clients_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('client_id');
+            $table->uuid('id')->primary(); // Change to UUID and set as primary key
+            $table->uuid('client_id')->index(); // Change to UUID
             $table->timestamps();
         });
     }
