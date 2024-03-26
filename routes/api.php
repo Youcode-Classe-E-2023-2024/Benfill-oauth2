fb345e3b0a93170b081241e19f38f29fc2bab4a0
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-26 11:38:02 +0000

    add swagger

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::middleware('auth:api')->group(function () {
-    Route::get('user', [AuthController::class, 'user']);
+    Route::get('users', [User::class, 'index']);
+    Route::post('users', [User::class, 'store']);
+    Route::put('users/{id}', [User::class, 'update']);
+    Route::delete('users/{id}', [User::class, 'destroy']);
 });
