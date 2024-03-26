2ac5566b72a48deda97583dc789bbb9fd1801a78
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-26 11:38:31 +0000

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,9 +14,12 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $admin = User::create([
             'name' => 'Admin',
             'email' => 'anass@gmail.com',
+            'password' => Hash::make('anass@gmail.com')
         ]);
+
+        $admin->assignRole('admin');
     }
 }
