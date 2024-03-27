06b896ecc8dbe7e7390cfc2c6e5e1e4a3bf83ee5
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-27 11:06:56 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Models\User;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\RolePermission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::middleware('auth:api')->group(function () {
-    Route::get('users', [User::class, 'index']);
-    Route::post('users', [User::class, 'store']);
-    Route::put('users/{id}', [User::class, 'update']);
-    Route::delete('users/{id}', [User::class, 'destroy']);
+Route::middleware(RolePermission::class)->group(function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('users', [UserController::class, 'store']);
+    Route::put('users/{id}', [UserController::class, 'update']);
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
 });
