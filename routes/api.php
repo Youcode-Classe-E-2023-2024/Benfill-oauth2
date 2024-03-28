48c9f57d3c8ce3d899116f18e81b33bf040bbbea
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-28 02:03:57 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::middleware(RolePermission::class)->group(function () {
-    Route::get('users', [UserController::class, 'index']);
-    Route::post('users', [UserController::class, 'store']);
-    Route::put('users/{id}', [UserController::class, 'update']);
-    Route::delete('users/{id}', [UserController::class, 'destroy']);
-    Route::post('assignRole', [UserController::class, 'assignRole']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->group(function () {
+    Route::middleware(RolePermission::class)->group(function () {
+        Route::get('users', [UserController::class, 'index']);
+        Route::get('user/{id}', [UserController::class, 'getUserDetails']);
+        Route::post('users', [UserController::class, 'store']);
+        Route::put('users/{id}', [UserController::class, 'update']);
+        Route::delete('users/{id}', [UserController::class, 'destroy']);
+        Route::post('assignRole', [UserController::class, 'assignRole']);
+
+    });
 });
