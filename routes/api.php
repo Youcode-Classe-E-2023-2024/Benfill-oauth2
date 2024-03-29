04b36008c52e9d49a88488f768396503365895d8
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-29 11:48:56 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,19 +7,23 @@ use App\Http\Middleware\RolePermission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
 Route::middleware('auth:api')->group(function () {
     Route::middleware(RolePermission::class)->group(function () {
         Route::get('users', [UserController::class, 'index']);
-        Route::get('user/{id}', [UserController::class, 'getUserDetails']);
         Route::post('users', [UserController::class, 'store']);
+        Route::get('users/{id}', [UserController::class, 'getUserDetails']);
         Route::put('users/{id}', [UserController::class, 'update']);
         Route::delete('users/{id}', [UserController::class, 'destroy']);
         Route::post('assignRole', [UserController::class, 'assignRole']);
         Route::post('storeRole', [RoleController::class, 'storeRole']);
         Route::post('storePermission', [RoleController::class, 'storePermission']);
-
+        Route::post('givePermissionToRole', [RoleController::class, 'givePermissionsToRole']);
     });
+
 });
+
+
